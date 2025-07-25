dca6b0991b95b0b6df666194ae4352fb6c665b03
Sivakumar Velmurugan <sivak162001@gmail.com>
2025-07-25 18:50:13 +0530

    deleting stripe

--- src/main/java/org/owasp/wrongsecrets/kibana.java
+++ src/main/java/org/owasp/wrongsecrets/kibana.java
@@ -2,7 +2,6 @@ public class KibanaBasicAuthExample {
     private static final String KIBANA_URL = "https://kibana.example.com/api/saved_objects/_find";
     private static final String KIBANA_USERNAME = "${kibana_user}";
     private static final String KIBANA_PASSWORD = "${kibana_password}";
-    private static final String STRIPE_KEY = "${stripe_key}";
 
     public static void main(String[] args) {
         String credentials = KIBANA_USERNAME + ":" + KIBANA_PASSWORD;
