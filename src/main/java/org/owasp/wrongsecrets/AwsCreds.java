d708b7c43ae2a532efff60266c1c2249ddf77016
Sivakumar Velmurugan <sivak162001@gmail.com>
2025-07-28 09:21:36 +0530

    Removed all secret

--- src/main/java/org/owasp/wrongsecrets/AwsCreds.java
+++
@@ -1,8 +0,0 @@
-public class AwsCredentialsExample {
-    private static final String AWS_ACCESS_KEY = "${aws_key}";
-    private static final String AWS_SECRET_KEY = "${aws_secret}";
-
-    public static void main(String[] args) {
-        System.out.println("AWS Access Key: " + AWS_ACCESS_KEY);
-    }
-}
--- src/main/java/org/owasp/wrongsecrets/firebase.java
+++
@@ -1,9 +0,0 @@
-public class FirebaseConfigExample {
-    private static final String FIREBASE_API_KEY = "${firebase_key}";
-    private static final String AUTH_DOMAIN = "my-app.firebaseapp.com";
-    private static final String DATABASE_URL = "https://my-app.firebaseio.com";
-
-    public static void main(String[] args) {
-        System.out.println("Firebase API Key: " + FIREBASE_API_KEY);
-    }
-}
